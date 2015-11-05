fd1acd566dd6e7364b7148c1dd6b52156decf151
egginsect <birdegg1357@gmail.com>
2015-11-05 14:11:09 +0800

    test file

---
+++ matlab/emotionStateTest.m
@@ -0,0 +1,31 @@
+clear all
+close all
+addpath(genpath('/media/hwlee/Data/dataset/CohnKanade+/emotionImages'));
+%%
+emotions = {'anger','contempt','disgust','fear','happy','sadness','surprise'}
+%%
+sn = StateNet();
+for i = 1:length(emotions)
+    load(emotions{i})
+    eval(['sn.createNewStateSeq(''', emotions{i}, ''',', emotions{i},'_images,',emotions{i},'_labels, 3, 10);']);
+    eval(['clear ', emotions{i}, '_images']);
+    eval(['clear ', emotions{i}, '_labels']);
+end
+%%
+sn.genNodes4graph()
+%s=sn.node2state('happy1')
+%%
+sn.computeMutualDist()
+%%
+g=sn.constructNNgraph(6);
+%g=sn.constructRelationGraph();
+%%
+%happyStates = StateSeq('happy',happy_images,happy_labels,5,10);
+
+%%
+%d=happyStates.computeDistances()
+%%
+%for i = 1:5
+    %happyStates.states{i}.showImgHorizontal(5)
+%end
+%dlmwrite('dist_happy_640_480.csv',d)
