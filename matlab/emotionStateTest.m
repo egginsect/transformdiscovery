062d695f0830529a35765938cf06b6ceeb99d23a
egginsect <birdegg1357@gmail.com>
2015-11-17 16:26:24 +0800

    seperateLoadEmotion

--- matlab/emotionStateTest.m
+++ matlab/emotionStateTest.m
@@ -1,23 +1,6 @@
-clear all
-close all
-imagepath='/media/hwlee/Data/dataset/CohnKanade+/emotionImages/';
+loadEmotion
 %%
-emotions = {'anger','contempt','disgust','fear','happy','sadness','surprise'}
-%%
-sn = StateNet();
-for i = 1:length(emotions)
-    load([imagepath,emotions{i}])
-    eval(['sn.addStateSeq(''', emotions{i}, ''',', emotions{i},'_images,',emotions{i},'_labels, 3, 10);']);
-    eval(['clear ', emotions{i}, '_images']);
-    eval(['clear ', emotions{i}, '_labels']);
-end
-%%
-%sn.computeMutualDist()
-%g=sn.showNNgraph(3);
-%%
-sn.mergeState({'anger1','contempt1','disgust1','fear1','happy1','sadness1','surprise1'},'Neutral');
-%%
-g=sn.showNNgraph(1);
+g=sn.showNNgraph(2);
 %%
 neutral = sn.getState('Neutral');
 %%
---
+++ matlab/loadEmotion.m
@@ -0,0 +1,18 @@
+clear all
+close all
+imagepath='/media/hwlee/Data/dataset/CohnKanade+/emotionImages/';
+%%
+emotions = {'anger','contempt','disgust','fear','happy','sadness','surprise'};
+%%
+sn = StateNet();
+for i = 1:length(emotions)
+    load([imagepath,emotions{i}])
+    eval(['sn.addStateSeq(''', emotions{i}, ''',', emotions{i},'_images,',emotions{i},'_labels, 3, 10);']);
+    eval(['clear ', emotions{i}, '_images']);
+    eval(['clear ', emotions{i}, '_labels']);
+end
+%%
+%sn.computeMutualDist()
+%g=sn.showNNgraph(3);
+%%
+sn.mergeState({'anger1','contempt1','disgust1','fear1','happy1','sadness1','surprise1'},'Neutral');
