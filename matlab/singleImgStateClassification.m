cc725ec438543b138ee016d5cc9197c405b2e773
egginsect <birdegg1357@gmail.com>
2015-11-18 17:33:28 +0800

    single image emotion recognition

--- matlab/singleImgStateClassification.m
+++ matlab/singleImgStateClassification.m
@@ -2,11 +2,18 @@ loadEmotionNet
 %%
 testImgs=cell(0);
 testEmotionLabels=[];
+Ntrial=10;
+accuracy = zeros(Ntrial,length(emotions));
+precision = zeros(Ntrial,length(emotions));
+recall = zeros(Ntrial,length(emotions));
+for trial=1:Ntrial
+testImgs=cell(0);
+testEmotionLabels=[];
 for emotionIdx=1:length(emotions)
 N=50;
-current_emotion = emotions{emotionIdx}
+current_emotion = emotions{emotionIdx};
 load([imagepath,current_emotion]);
-idx=partitionImageSeqs(labels,3);
+idx=partitionImageSeqs(labels,5);
 c=ismember(idx{end},usedImgIdx{emotionIdx});
 idx{end}(find(c))=[];
 Imgs = images(idx{end});
@@ -16,26 +23,39 @@ testEmotionLabels = [testEmotionLabels; emotionIdx*ones(N,1)];
 clear images
 clear labels
 end
-%%
+%
+strs=cell(0);
 for emotionIdx=1:7
-%for emotionIdx=1:length(emotions)
-count=0;
-pcount=0;
-    for j=1:length(testImgs)
-     str = sn.findRelevantState(Imgs{j}(:));
+current_emotion = emotions{emotionIdx};
+TpCount=0;
+TnCount=0;
+pCount=0;
+nCount=0;
+negativeIdx = find(testEmotionLabels~=emotionIdx);
+p=randperm(length(negativeIdx));
+NewtestEmotionLabels = [-ones(50,1);ones(50,1)];
+NewtestImgs = testImgs([negativeIdx(p(1:50));find(testEmotionLabels==emotionIdx)]);
+    for j=1:length(NewtestImgs)
+     str = sn.findRelevantState(NewtestImgs{j}(:));
+     strs{j,emotionIdx}=str;
      if(findstr(str,current_emotion))
-         if(testEmotionLabels(j)==emotionIdx)
-             count=count+1;
-             pcount=pcount+1;
+         pCount = pCount+1;
+         if(NewtestEmotionLabels(j)==1)
+             TpCount=TpCount+1;
          end
      else
-         if(testEmotionLabels(j)~=emotionIdx)
-             count=count+1;
+         nCount=nCount+1;
+         if(NewtestEmotionLabels(j)==-1)
+             TnCount=TnCount+1;
          end
      end
+     
     end
  %end
-accuracy(emotionIdx) =count/length(testImgs)*100;
-precision(emotionIdx) =pcount/N*100;
+accuracy(trial,emotionIdx) =(TpCount+TnCount)/length(NewtestImgs)*100;
+precision(trial,emotionIdx) = TpCount/pCount*100;
+recall(trial,emotionIdx) = TpCount/N*100;
+%dlmwrite('accuracy_dist_to_principal_direction.csv',accuracy)
 end
-%%
+end
+dlmwrite('reconstructionSimilarity_no_transient.csv',[mean(accuracy,1);mean(precision,1);mean(recall,1)])
